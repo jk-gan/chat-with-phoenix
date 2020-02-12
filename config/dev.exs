73baa8fdc6f43eb9ee4bd545afa053cd6bc9f772
Gan Jun Kai <kuhn96@gmail.com>
2020-02-12 17:06:57 +0800

    chore: use my local pg config

--- config/dev.exs
+++ config/dev.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 # Configure your database
 config :chat_app, ChatApp.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "jkgan",
+  password: "",
   database: "chat_app_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
