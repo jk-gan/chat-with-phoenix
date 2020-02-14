cc63e4984eb9efe2adc45beb88997de56a350f0f
Gan Jun Kai <kuhn96@gmail.com>
2020-02-14 23:48:55 +0800

    feat: add some stylings for new room page

--- lib/chat_app_web/controllers/room_controller.ex
+++ lib/chat_app_web/controllers/room_controller.ex
@@ -1,7 +1,14 @@
 defmodule ChatAppWeb.RoomController do
   use ChatAppWeb, :controller
+
+  alias ChatApp.Talk.Room
    
   def index(conn, _params) do
     render(conn, "index.html")
+  end
+
+  def new(conn, _params) do
+    changeset = Room.changeset(%Room{}, %{})
+    render(conn, "new.html", changeset: changeset)
   end
 end
